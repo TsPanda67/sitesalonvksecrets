c7f20e3c884cd822cae99868bf778997b5040293
TsPanda <khajem672@gmail.com>
2025-07-06 18:58:58 +0200

    correction-planity-rdv

---
+++ src/types/planity.d.ts
@@ -0,0 +1,11 @@
+interface Window {
+  planity: {
+    key: string;
+    primaryColor: string;
+    container?: HTMLElement;
+    accountContainer?: HTMLElement;
+    appointmentContainer?: HTMLElement;
+    giftVoucherContainer?: HTMLElement;
+    onlineShopContainer?: HTMLElement;
+  };
+}
